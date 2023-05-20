ea7b3bf969b240d2be7e2c0f829b6c48d32c76a2
Stefan Bayne <stefanbusiness95@gmail.com>
2023-05-20 15:42:45 -0400

    refactored to add binding to adapter.

--- app/src/main/java/com/example/stefanbayneitunes/view/ItunesSongAdapter.kt
+++ app/src/main/java/com/example/stefanbayneitunes/view/ItunesSongAdapter.kt
@@ -6,13 +6,12 @@ import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.stefanbayneitunes.model.AllDataOfTheSongs
 import com.example.stefanbayneitunes.R
+import com.example.stefanbayneitunes.databinding.ImageForItunesSongBinding
 
 
 class ItunesSongAdapter(private val list: List<AllDataOfTheSongs>) :
@@ -21,59 +20,48 @@ class ItunesSongAdapter(private val list: List<AllDataOfTheSongs>) :
     // Inner class to begin the adaptation of our data source to our UI
     inner class SongViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        // method to bind all the views in the layout to the recyclerView
         @SuppressLint("SetTextI18n")
         fun onBind(iTunesSong: AllDataOfTheSongs) {
-            val tvArtist: TextView = itemView.findViewById(R.id.name_Of_Artist)
-            val tvCollection: TextView = itemView.findViewById(R.id.genre_type)
-            val tvSong: TextView = itemView.findViewById(R.id.name_Of_Song)
-            val tvSongPrice: TextView = itemView.findViewById(R.id.price_Of_Song)
-            val ivUserThumbnail: ImageView = itemView.findViewById(R.id.song_image)
-            val primaryGenre = itemView.findViewById<TextView>(R.id.primary_genre)
+            val binding = ImageForItunesSongBinding.bind(itemView)
 
-            // Assigning the views to the itunes information
-            tvArtist.text = iTunesSong.artistName
-            tvCollection.text = iTunesSong.collectionName
-            tvSong.text = iTunesSong.trackName
-            primaryGenre.text = iTunesSong.primaryGenreName
+            binding.apply {
+                artistName.text = iTunesSong.artistName
+                collectionName.text = iTunesSong.collectionName
+                trackName.text = iTunesSong.trackName
+                primaryGenre.text = iTunesSong.primaryGenreName
 
-            // assigning the price of each song
-            if(iTunesSong.trackPrice.isNullOrBlank()) {
-                tvSongPrice.text = "**FREE**"
-            } else {
-                tvSongPrice.text = "$${iTunesSong.trackPrice} / $${iTunesSong.collectionPrice}"
-            }
+                if(iTunesSong.trackPrice.isNullOrBlank()) {
+                    songPrice.text = "**FREE**"
+                } else {
+                    songPrice.text = "$${iTunesSong.trackPrice} / $${iTunesSong.collectionPrice}"
+                }
 
-            // Loading the image into the recyclerView with Picasso
-            Glide.with(itemView.context)
-                .load(iTunesSong.artworkUrl60)
-                .placeholder(R.drawable.baseline_music_note_24)
-                .centerCrop()
-                .transition(DrawableTransitionOptions.withCrossFade())
-                .into(ivUserThumbnail)
+                Glide.with(itemView.context)
+                    .load(iTunesSong.artworkUrl60)
+                    .placeholder(R.drawable.baseline_music_note_24)
+                    .centerCrop()
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(songImage)
 
-            // Making the itunes song able to be played with the previewUrl
-            itemView.setOnClickListener {
-                val intent = Intent()
-                intent.action = Intent.ACTION_VIEW
-                intent.setDataAndType(Uri.parse(iTunesSong.previewUrl), "audio/*")
-                itemView.context.startActivity(intent)
+                itemView.setOnClickListener {
+                    val intent = Intent()
+                    intent.action = Intent.ACTION_VIEW
+                    intent.setDataAndType(Uri.parse(iTunesSong.previewUrl), "audio/*")
+                    itemView.context.startActivity(intent)
+                }
             }
         }
     }
 
-    // Creates the ViewHolder and inflates the layout
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
         val listItem = LayoutInflater.from(parent.context)
             .inflate(R.layout.image_for_itunes_song, parent, false)
         return SongViewHolder(listItem)
     }
 
-    // Binds the viewHolder to the position
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) =
         holder.onBind(list[position])
 
-    // returns the item count in the recyclerView
     override fun getItemCount(): Int = list.size
 
 }
