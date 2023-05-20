7ad2199828d80648df3f8c61b80bad17adb22c04
Stefan Bayne <stefanbusiness95@gmail.com>
2023-05-20 13:23:29 -0400

    adding an interceptor and encapsulating gson.

--- app/src/main/java/com/example/stefanbayneitunes/api/APIForItunes.kt
+++ app/src/main/java/com/example/stefanbayneitunes/api/APIForItunes.kt
@@ -1,11 +1,14 @@
 package com.example.stefanbayneitunes.api
 
 import com.example.stefanbayneitunes.model.DataForSongs
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.concurrent.TimeUnit
 
 interface ApiServiceITunes {
 
@@ -24,10 +27,27 @@ interface ApiServiceITunes {
         private const val BASE_URL = "https://itunes.apple.com/"
         private var instance: Retrofit? = null
 
+        private fun provideHttpInterceptor(): OkHttpClient {
+            val interceptor = HttpLoggingInterceptor().apply {
+                this.level = HttpLoggingInterceptor.Level.BODY
+            }
+
+            val client = OkHttpClient.Builder().apply {
+                this.addInterceptor(interceptor)
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .readTimeout(10, TimeUnit.SECONDS)
+                    .writeTimeout(10, TimeUnit.SECONDS)
+            }.build()
+            return client
+        }
+
         fun createRetrofit(): Retrofit {
+            val gson: GsonConverterFactory = GsonConverterFactory.create()
+
             if (instance == null) {
                 instance = Retrofit.Builder().baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(gson)
+                    .client(provideHttpInterceptor())
                     .build()
             }
 
